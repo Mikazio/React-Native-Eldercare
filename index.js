e7dffcca274e89d184f8c34df4267700a1982bef
Nattanicha <natchajang01@gmail.com>
2018-08-30 18:02:20 +0700

    [ADD]Home screen

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /** @format */
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import Header from './src/components/Header';
-import CardList from './src/components/CardList';
+import Home from './src/screens/Home';
+
 
 const App = () => (
   <View>
-    <Header headerText={'Eldercare'} />
-    <CardList />
+    <Home />
   </View>
 );
 
--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { ScrollView, Text } from 'react-native';
 
 const CardDetails = () => (
-    <ScrollView>
+    
       <Text>This is Card Details Section</Text>
-    </ScrollView>
+
   );
 
 export default CardDetails;

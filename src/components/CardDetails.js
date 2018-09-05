6ac820a07d2d9cf9a4097feab4a405ef8b87d738
Unknown <varin.mikaz@mail.kmutt.ac.th>
2018-09-05 15:52:40 +0700

    [ADD]Data Mapping in CardDetails

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { ScrollView, Text } from 'react-native';
 
-const CardDetails = ({ data }) => (
+const CardDetails = ({ data }) => {
+    const { DeviceId, timestamp } = data;
+    const { XMag,
+            YMag,
+            ZMag,
+            Heading,
+            Pitch,
+            Roll,
+            XAxis,
+            YAxis,
+            ZAxis
+          } = data.data;
+
+    return (
     <ScrollView>
-      <Text>{data.DeviceId}</Text>
+      <Text>Xmag= {XMag}</Text>
+      <Text>Ymag= {YMag}</Text>
+      <Text>Zmag= {ZMag}</Text>
+      <Text>Heading= {Heading}</Text>
+      <Text>Pitch= {Pitch}</Text>
+      <Text>Roll= {Roll}</Text>
+      <Text>XAxis= {XAxis}</Text>
+      <Text>YAxis= {YAxis}</Text>
+      <Text>ZAxis= {ZAxis}</Text>
+      <Text>DeviceId= {DeviceId}</Text>
+      <Text>timestamp= {timestamp}</Text>
     </ScrollView>
   );
+  };
 
 export default CardDetails;

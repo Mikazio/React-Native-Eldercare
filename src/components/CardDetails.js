20f20fac206635a4c1b7f45d07c6215e46081d0e
Nattanicha <natchajang01@gmail.com>
2018-09-06 22:59:26 +0700

    [ADD]Card

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
 const CardDetails = ({ data }) => {
     const { DeviceId, timestamp } = data;
@@ -15,20 +16,20 @@ const CardDetails = ({ data }) => {
           } = data.data;
 
     return (
-    <ScrollView>
-      <Text>Xmag= {XMag}</Text>
-      <Text>Ymag= {YMag}</Text>
-      <Text>Zmag= {ZMag}</Text>
-      <Text>Heading= {Heading}</Text>
-      <Text>Pitch= {Pitch}</Text>
-      <Text>Roll= {Roll}</Text>
-      <Text>XAxis= {XAxis}</Text>
-      <Text>YAxis= {YAxis}</Text>
-      <Text>ZAxis= {ZAxis}</Text>
-      <Text>DeviceId= {DeviceId}</Text>
-      <Text>timestamp= {timestamp}</Text>
-    </ScrollView>
+    <Card>
+        <Text>Xmag= {XMag}</Text>
+        <Text>Ymag= {YMag}</Text>
+        <Text>Zmag= {ZMag}</Text>
+        <Text>Heading= {Heading}</Text>
+        <Text>Pitch= {Pitch}</Text>
+        <Text>Roll= {Roll}</Text>
+        <Text>XAxis= {XAxis}</Text>
+        <Text>YAxis= {YAxis}</Text>
+        <Text>ZAxis= {ZAxis}</Text>
+        <Text>DeviceId= {DeviceId}</Text>
+        <Text>timestamp= {timestamp}</Text>
+    </Card>
   );
-  };
+ };
 
 export default CardDetails;

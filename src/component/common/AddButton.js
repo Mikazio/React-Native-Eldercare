8375834dbc661efbc42ca149a37ebdc5abdd209b
Mikaz <varin.mikaz@mail.kmutt.ac.th>
2018-09-07 14:26:02 +0700

    [ADD]React-Navigation Dependencies

--- src/component/common/AddButton.js
+++ src/component/common/AddButton.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
 
-const AddButton = () => {
-  return (
+const AddButton = () => (
     <TouchableOpacity>
       <View style={{ backgroundColor: 'blue', borderRadius: 100 }}>
         <Icon name='plus' />
       </View>
     </TouchableOpacity>
   );
-};
 
 export default AddButton;

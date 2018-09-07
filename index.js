00bb1a52ea87b632fc824cfcb38ef5dd1cdba063
Nattanicha <natchajang01@gmail.com>
2018-09-07 13:38:50 +0700

    [ADD]HomeScreen

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { AppRegistry, ScrollView } from 'react-native';
 import Header from './src/components/Header';
 import CardList from './src/components/CardList';
+import Home from './src/components/Home';
 
 const App = () => (
   <ScrollView>
     <Header headerText={'Eldercare'} />
-    <CardList />
+    <Home />
   </ScrollView>
 );
 

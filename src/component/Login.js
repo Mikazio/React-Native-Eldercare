58c0e6ebf32c020d81869a8f7be807b7a7db776a
Nattanicha <natchajang01@gmail.com>
2018-09-08 17:58:22 +0700

    [Edit]UI Login

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
+//import { View } from 'react-native';
+import { Container, Content, Form, Item, Input, Label, Button, Text, Footer } from 'native-base';
 
 export default class Login extends Component {
   render() {
@@ -12,21 +13,29 @@ export default class Login extends Component {
               <Label>Username</Label>
               <Input />
             </Item>
-            <Item floatingLabel last>
+            <Item floatingLabel>
               <Label>Password</Label>
               <Input />
             </Item>
           </Form>
-          <Button transparent light>
+          <Button transparent info>
             <Text>Forget Password?</Text>
           </Button>
-          <Button block>
+          <Button block bordered>
             <Text>Login</Text>
           </Button>
           <Text>OR</Text>
           <Button block>
             <Text>Login with Facebook</Text>
           </Button>
+        </Content>
+        <Content>
+          <Footer style={{ backgroundColor: '#ddd' }}>
+            <Text>Do not have an account? </Text>
+            <Button transparent small info>
+              <Text>Register.</Text>
+            </Button>
+          </Footer>
         </Content>
       </Container>
     );

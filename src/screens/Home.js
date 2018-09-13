a1887e9d706df748427794399588cf66f06509d2
Nattanicha <natchajang01@gmail.com>
2018-09-13 17:24:19 +0700

    [ADD]Card wearer

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Text } from 'native-base';
+import { View, TouchableOpacity } from 'react-native';
+import { Button, Text, Card, CardItem, Body } from 'native-base';
 
 export default class Home extends Component {
 
@@ -11,7 +11,27 @@ export default class Home extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Text>HomeScreen</Text>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('Watch')}>
+          <Card style={{ height: 228, width: 172 }} >
+            <CardItem style={{ paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0 }}>
+                <View style={{ height: 175, width: null, flex: 1 }}>
+                  <Text>Image</Text>
+                </View>
+            </CardItem>
+            <CardItem style={{ backgroundColor: '#16879E', paddingTop: 6, paddingLeft: 8 }} >
+              <Body>
+                <Text>Name Lastname</Text>
+                <Text note>id58130500026</Text>
+              </Body>
+            </CardItem>
+          </Card>
+        </TouchableOpacity>
+
+        </View>
+
         <View style={styles.layoutButtom}>
           <Button
               onPress={() => this.props.navigation.navigate('Watch')}

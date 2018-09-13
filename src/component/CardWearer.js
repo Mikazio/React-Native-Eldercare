ab3167e520278b79647300a480dd32233d44cff4
Unknown <varin.mikaz@mail.kmutt.ac.th>
2018-09-13 19:52:38 +0700

    [Edit]De-Structure CardWearer

--- src/component/CardWearer.js
+++ src/component/CardWearer.js
@@ -1,1 +1,23 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { Text, Card, CardItem, Body } from 'native-base';
 
+export default class CardWearer extends Component {
+  render() {
+    return (
+        <Card style={{ height: 228, width: 172 }} >
+          <CardItem style={{ paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0 }}>
+              <View style={{ height: 175, width: null, flex: 1 }}>
+                <Text>Image</Text>
+              </View>
+          </CardItem>
+          <CardItem style={{ backgroundColor: '#16879E', paddingTop: 6, paddingLeft: 8 }} >
+            <Body>
+              <Text>Name Lastname</Text>
+              <Text note>id58130500026</Text>
+            </Body>
+          </CardItem>
+        </Card>
+    );
+  }
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity } from 'react-native';
-import { Button, Text, Card, CardItem, Body } from 'native-base';
+import { Button, Text } from 'native-base';
+
+import CardWearer from '../component/CardWearer';
 
 export default class Home extends Component {
 
@@ -11,25 +13,10 @@ export default class Home extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-
         <View style={{ flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }}>
-
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Watch')}>
-          <Card style={{ height: 228, width: 172 }} >
-            <CardItem style={{ paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0 }}>
-                <View style={{ height: 175, width: null, flex: 1 }}>
-                  <Text>Image</Text>
-                </View>
-            </CardItem>
-            <CardItem style={{ backgroundColor: '#16879E', paddingTop: 6, paddingLeft: 8 }} >
-              <Body>
-                <Text>Name Lastname</Text>
-                <Text note>id58130500026</Text>
-              </Body>
-            </CardItem>
-          </Card>
-        </TouchableOpacity>
-
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('Watch')}>
+              <CardWearer />
+            </TouchableOpacity>
         </View>
 
         <View style={styles.layoutButtom}>

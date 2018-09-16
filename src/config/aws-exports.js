6e9a1318fc1f112b3f3b3865209c81136fbce356
Unknown <varin.mikaz@mail.kmutt.ac.th>
2018-09-16 20:51:54 +0700

    [Edit]Redirect to amazon services

--- src/config/aws-exports.js
+++ src/config/aws-exports.js
@@ -1,8 +1,8 @@
 const config = {
-  identityPoolId: 'ap-southeast-1:837e9fd3-0a12-445f-b0a6-3dee8cb5ada9',
+  identityPoolId: 'ap-southeast-1:cdb0d2fa-da61-457c-abd2-c9bc21c12e28',
   region: 'ap-southeast-1',
-  userPoolId: 'ap-southeast-1_d4xuuWoua',
-  userPoolWebClientId: '7bu0tnns2i44ir7ig8dfa166k9'
+  userPoolId: 'ap-southeast-1_EkOEEibc0',
+  userPoolWebClientId: '3lq6kmec08of5u8bm3fh3jjirg'
 };
 
 export default config;

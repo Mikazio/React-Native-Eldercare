3d446fe3b52bf011e89caf1b5a48b3d13ddb8546
Nattanicha <natchajang01@gmail.com>
2018-09-19 22:08:27 +0700

    [EDIT] add UI

--- src/screens/ForgetPassword.js
+++ src/screens/ForgetPassword.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Content, Card, CardItem, Body, Text } from 'native-base';
+import { View } from 'react-native';
+import { Container, Item, Text, Button, Input, Label } from 'native-base';
 
 export default class ForgetPassword extends Component {
 
@@ -9,7 +10,24 @@ export default class ForgetPassword extends Component {
 
   render() {
     return (
-      <Text>ForgetPassword</Text>
+      <Container style={{ alignItems: 'center' }}>
+        <Container style={{ width: 320 }}>
+            <View style={{ paddingBottom: 16, paddingTop: 16 }}>
+              <Item floatingLabel>
+                <Label>Enter your email address</Label>
+                <Input />
+              </Item>
+            </View>
+            <View>
+              <Button
+                block
+                onPress={() => this.props.navigation.navigate('ResetPassword')}
+              >
+                <Text>Send Login Link</Text>
+              </Button>
+            </View>
+        </Container>
+      </Container>
     );
   }
 }

2cbc329c80ccf123c6f46830625cf1b595d01850
Unknown <varin.mikaz@mail.kmutt.ac.th>
2018-09-19 20:28:25 +0700

    [Add]Forgetpass and confirmsignup screens

---
+++ src/screens/ConfirmSignUp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container, Content, Card, CardItem, Body, Text } from 'native-base';
+
+export default class ConfirmSignUp extends Component {
+
+  static navigationOptions = {
+    title: 'ConfirmSignUp',
+  };
+
+  render() {
+    return (
+      <Text>ConfirmSignUp</Text>
+    );
+  }
+}
---
+++ src/screens/ForgetPassword.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container, Content, Card, CardItem, Body, Text } from 'native-base';
+
+export default class ForgetPassword extends Component {
+
+  static navigationOptions = {
+    title: 'ForgetPassword',
+  };
+
+  render() {
+    return (
+      <Text>ForgetPassword</Text>
+    );
+  }
+}

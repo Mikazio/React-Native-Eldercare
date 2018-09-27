5070ad99eace67816faef6b2e12402c8750790d4
Nattanicha <natchajang01@gmail.com>
2018-09-27 18:31:42 +0700

    [EDIT] header color

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Content, Form, Item, Input, Label, Button, Text, Footer } from 'native-base';
+import { Image, View } from 'react-native';
+import { Container, Content, Form, Item, Input, Label, Button, Text, Footer, Right, Header, Left, Icon, Body, Title } from 'native-base';
 import Amplify, { Auth } from 'aws-amplify';
 import awsmobile from '../../aws-exports';
 
@@ -9,6 +10,7 @@ export default class Login extends Component {
 
   static navigationOptions = {
     title: 'Login',
+    header: null
   };
 
   state = {
@@ -41,11 +43,27 @@ export default class Login extends Component {
 
   render() {
     return (
-      <Container style={{ alignItems: 'center' }}>
-        <Content style={{ width: '90%' }}>
+      <Container style={styles.container}>
+      <Header
+        androidStatusBarColor='#168297'
+        style={{ backgroundColor: '#16879E', width: '100%' }}
+      >
+
+       <Body style={{ alignItems: 'center' }}>
+         <Title>Eldercare</Title>
+       </Body>
+
+     </Header>
+        <Content style={styles.content}>
+          <View style={styles.photo_container}>
+            <Image
+              source={{ uri: 'https://scontent.fbkk2-1.fna.fbcdn.net/v/t1.0-9/10406994_854632824607247_2048936579886851496_n.jpg?_nc_cat=111&oh=3b759dad66550d6680db530e9ca5bc65&oe=5C34B546' }}
+              style={styles.photo}
+            />
+          </View>
           <Form>
-            <Item floatingLabel last>
-              <Label>Username</Label>
+            <Item floatingLabel last >
+              <Label style={{ color: '#3C436A' }}>Email</Label>
               <Input
                 value={this.state.username}
                 onChangeText={value => this.setState({ username: value })}
@@ -53,7 +71,7 @@ export default class Login extends Component {
             </Item>
 
             <Item floatingLabel last>
-              <Label>Password</Label>
+              <Label style={{ color: '#3C436A' }}>Password</Label>
               <Input
               secureTextEntry
               value={this.state.password}
@@ -61,38 +79,47 @@ export default class Login extends Component {
               />
             </Item>
           </Form>
-          <Button
-            transparent info
-            onPress={() => this.props.navigation.navigate('ForgetPassword')}
-          >
-            <Text>Forget Password?</Text>
-          </Button>
+
+          <View style={{ paddingBottom: '3%', width: 500 }}>
+            <Right>
+              <Button
+                transparent info
+                onPress={() => this.props.navigation.navigate('ForgetPassword')}
+              >
+                <Text style={{ color: '#16879E' }}>Forget Password?</Text>
+              </Button>
+            </Right>
+          </View>
 
           <Button
             block
             onPress={this.signIn.bind(this)}
+            style={{ backgroundColor: '#16879E' }}
           >
             <Text>Login</Text>
           </Button>
 
-          <Text>OR</Text>
+          <View style={{ alignItems: 'center', padding: '3%' }}>
+            <Text style={{ color: '#979A9A' }}>OR</Text>
+          </View>
 
           <Button
             block bordered
             onPress={() => this.props.navigation.navigate('RegisterWearer')}
+            style={{ borderColor: '#16879E' }}
           >
-            <Text>Register device</Text>
+            <Text style={{ color: '#16879E' }}>Register device</Text>
           </Button>
 
         </Content>
 
-          <Footer style={{ backgroundColor: '#ddd', width: '100%' }}>
-            <Text>Do not have an account? </Text>
+          <Footer style={styles.footer}>
+            <Text style={{ color: '#979A9A', paddingTop: 3 }}>Do not have an account? </Text>
             <Button
               transparent small info
               onPress={() => this.props.navigation.navigate('Register')}
             >
-              <Text>Register.</Text>
+              <Text style={{ color: '#16879E' }}>Register.</Text>
             </Button>
           </Footer>
 
@@ -100,3 +127,31 @@ export default class Login extends Component {
     );
   }
 }
+const styles = {
+  container: {
+    alignItems: 'center',
+
+  },
+  content: {
+    width: '90%'
+  },
+  photo_container: {
+    alignItems: 'center',
+    height: 110,
+    width: null,
+    marginTop: 35,
+  },
+
+  photo: {
+    alignItems: 'center',
+    height: 100,
+    width: 100,
+    borderRadius: 100
+  },
+  footer: {
+    backgroundColor: '#FFFF',
+    paddingTop: 12,
+    width: '100%'
+  }
+
+};

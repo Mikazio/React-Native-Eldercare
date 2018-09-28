3dbbd26eef29859604722fa59310d8b2fe0c036c
Nattanicha <natchajang01@gmail.com>
2018-09-28 17:54:13 +0700

    [EDIT] header, card, color

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react';
 import { TouchableOpacity, View } from 'react-native';
-import { Container, Button, Icon } from 'native-base';
+import { Container, Button, Icon, Header, Left, Body, Right, Title, Text } from 'native-base';
 import CardWearer from '../component/CardWearer';
 
 export default class Home extends Component {
 
   static navigationOptions = {
     title: 'Home',
+    header: null
   };
 
   render() {
     return (
       <Container style={styles.container}>
+        <Header
+          androidStatusBarColor='#168297'
+          style={{ backgroundColor: '#16879E' }}
+        >
+            <Left>
+              <Button transparent>
+                <Icon name="menu" />
+              </Button>
+            </Left>
+            <Body>
+              <Title>Eldercare</Title>
+            </Body>
+            <Right>
+              <Button
+                transparent
+                onPress={() => this.props.navigation.navigate('SearchWearer')}
+              >
+              <Text>ADD</Text>
+            </Button>
+            </Right>
+          </Header>
         <View style={styles.layout_card}>
 
             <TouchableOpacity onPress={() => this.props.navigation.navigate('Watch')}>
               <CardWearer />
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('Watch')}>
-              <CardWearer />
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('Watch')}>
-              <CardWearer />
-            </TouchableOpacity>
-
         </View>
-          <Button
-            style={{ backgroundColor: 'blue', borderRadius: 100 }}
-            onPress={() => this.props.navigation.navigate('SearchWearer')}
-          >
-            <Icon type='EvilIcons' name='plus' />
-          </Button>
       </Container>
     );
   }
@@ -41,7 +49,6 @@ export default class Home extends Component {
 const styles = {
   container: {
     flex: 1,
-    padding: 3
   },
   layout_card: {
     flexDirection: 'row',

71c585dd23947df84617f76fac8dc7eb7797f67b
Mikaz <varin.mikaz@mail.kmutt.ac.th>
2018-10-03 22:38:01 +0700

    [Edit]Hide-able WearerCard

--- src/screens/SearchWearer.js
+++ src/screens/SearchWearer.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Image, View } from 'react-native';
+import { View } from 'react-native';
 import { Auth, API } from 'aws-amplify';
-import { Container, Content, Item, Input, Label, Button, Text, Card, CardItem, Form, Icon, Header, Left, Body, Right, Title } from 'native-base';
+import { Container, Content, Item, Input, Label, Button, Text, Form, Icon, Header, Left, Body, Right, Title } from 'native-base';
+import WearerCard from '../component/WearerCard';
+
 
 export default class SearchWearer extends Component {
 
@@ -26,25 +28,17 @@ export default class SearchWearer extends Component {
   }
 
   async searchForWearer(WearerId) {
-    await API.get('WearerTableCRUD', '/WearerTable/' + WearerId)
-      .then(res => this.setState({
-        WearerData: res
+    await API.get('WearerTableCRUD', `/WearerTable/${WearerId}`)
+      .then(data => this.setState({
+        WearerData: data
       }))
       .catch(err => console.log('err', err.response));
   }
 
-  async testSearch() {
-    let apiName = 'NewWearerCRUD';
-    let path = '/NewWearer/object/' + this.state.WearerId;
-    let myInit = {
-      queryStringParameters: {
-        gender: 'Female'
-      }
+  displayCard() {
+    if (this.state.WearerData.length > 0) {
+      return <WearerCard />;
     }
-    console.log('current path ', path);
-    await API.get(apiName, path)
-      .then(response => console.log(response))
-      .catch(error => { console.log(error.response); });
   }
 
   render() {
@@ -87,45 +81,11 @@ export default class SearchWearer extends Component {
                 <Text>Check DeviceID</Text>
               </Button>
             </View>
-
-
-        <View style={{ paddingTop: 50, alignItems: 'center' }}>
-
-          <Card style={{ height: 280, width: 220, alignItems: 'center' }} >
-            <CardItem header>
-              <Text>DeviceID: </Text>
-              <Text>58130500026</Text>
-            </CardItem>
-            <CardItem style={{ paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0 }}>
-                <Image
-                  source={{ uri: 'https://scontent.fbkk2-1.fna.fbcdn.net/v/t1.0-9/10406994_854632824607247_2048936579886851496_n.jpg?_nc_cat=111&oh=3b759dad66550d6680db530e9ca5bc65&oe=5C34B546' }}
-                  style={styles.photo}
-                />
-            </CardItem>
-            <CardItem>
-                <Text>Name Lastname</Text>
-            </CardItem>
-            <CardItem>
-              <Button
-                style={{ backgroundColor: '#16879E' }}
-                onPress={() => console.log(this.state.WearerData)}
-              >
-                <Text>Add</Text>
-              </Button>
-            </CardItem>
-          </Card>
-
-        </View>
+            <View>
+              {this.displayCard()}
+            </View>
         </Content>
       </Container>
     );
   }
 }
-const styles = {
-  photo: {
-    alignItems: 'center',
-    height: 100,
-    width: 100,
-    borderRadius: 100
-  }
-};
